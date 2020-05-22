c3ddd1e4d49dcae6f13b5c3a8555d75b38b41944
pietscmi <michael.pietschmann@student.reutlingen-university.de>
2020-05-22 23:20:18 +0200

    adjusted detect-language and inserted necessary code (with Manuel and Ebru)

--- src/detect-language.js
+++ src/detect-language.js
@@ -36,7 +36,7 @@ function main(params) {
   return new Promise(function (resolve, reject) {
 
     try {
-      
+
       // *******TODO**********
       // - Call the language identification API of the translation service
       // see: https://cloud.ibm.com/apidocs/language-translator?code=node#identify-language
@@ -47,16 +47,46 @@ function main(params) {
       // in case of errors during the call resolve with an error message according to the pattern 
       // found in the catch clause below
 
-      resolve({
-        statusCode: 200,
-        body: {
-          text: params.text, 
-          language: "<Best Language>",
-          confidence: 0.5,
-        },
-        headers: { 'Content-Type': 'application/json' }
+      const languageTranslator = new LanguageTranslatorV3({
+        version: '2018-05-01',
+        authenticator: new IamAuthenticator({
+          apikey: params.apikey,
+        }),
+        url: params.url,
       });
 
+      const defaultText = {
+        text: 'Language translator translates text from one language to another. But no Text was given.' +
+            ' In die Webbrowser try "?text=your text" to translate your own text.'
+      };
+
+      var objWithTextAttr;
+      if('text' in params && params.text !== undefined && params.text !== null) {
+        objWithTextAttr = params;
+      }else {
+        objWithTextAttr = defaultText;
+      }
+
+      languageTranslator.identify(objWithTextAttr)
+          .then(identifiedLanguages => {
+            console.log(JSON.stringify(identifiedLanguages, null, 2));
+            console.log("form inside identify:");
+            console.log(params);
+            resolve({
+              statusCode: 200,
+              body: {
+                text: objWithTextAttr.text,
+                language: identifiedLanguages.result.languages[0].language,
+                confidence: identifiedLanguages.result.languages[0].confidence,
+              },
+              headers: { 'Content-Type': 'application/json' }
+            });
+          })
+          .catch(err => {
+            console.log('error:', err);
+            resolve(getTheErrorResponse('Could not identify language', defaultLanguage));
+
+          });
 
     } catch (err) {
       console.error('Error while initializing the AI service', err);
